dfd01f1671e73a5f6ec58b44d412efbfd1898c89
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-11-05 20:46:26 +0530

    sign up debugged

--- authapp/views.py
+++ authapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def Home(request):
@@ -8,22 +9,39 @@ def Home(request):
 
 def signup(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        phone = request.POST['usernumber']
+        name = request.POST['fullname']
+        phone = request.POST['phone']
         email = request.POST['email']
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
+
 
         if pass1 != pass2:
             messages.info(request, "Password is not Matching")
             return redirect('signup')
 
-        myUser = User.objects.create_user(name, email, pass1)
+        myUser = User.objects.create_user(username=phone,email= email, password=
+         pass1)
+        myUser.fullname = name
         myUser.save()
         messages.success(request, "User is created successfully")
         return redirect('login')
           
     return render(request, "signup.html")
 
+
 def user_login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pass1']
+        myUser = authenticate(request, email=email, pass1=password)
+
+        if myUser is not None:
+            login(request, myUser)
+            messages.success(request, "Login Successful")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid Credentials")
+            return redirect('login')
+
     return render(request, "login.html")
